ab575549250d3820b85efdd9ed68e9841666fabb
enesreha <enesreha@gmail.com>
2019-02-11 19:35:01 +0300

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Enes Reha GILBAZ on 2/11/19.
+//  Copyright © 2019 Enes Reha GILBAZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
